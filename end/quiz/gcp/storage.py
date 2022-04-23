20289efafcfad25ed6a82d14cc3d8778288f3ecb
asia flowers <90466041+asiaflowers@users.noreply.github.com>
2022-04-23 18:07:39 -0500

    Update storage.py

--- end/quiz/gcp/storage.py
+++ end/quiz/gcp/storage.py
@@ -1,1 +1,62 @@
 
+
+import os
+project_id = os.getenv('GCLOUD_PROJECT')
+
+# TODO: Get the Bucket name from the GCLOUD_BUCKET environment variable
+
+bucket_name = os.getenv('GCLOUD_BUCKET')
+
+# END TODO
+
+# TODO: Import the storage module
+
+from google.cloud import storage
+
+# END TODO
+
+# TODO: Create a client for Cloud Storage
+
+storage_client = storage.Client()
+
+# END TODO
+
+# TODO: Use the client to get the Cloud Storage bucket
+
+bucket = storage_client.get_bucket(bucket_name)
+
+# END TODO
+
+"""
+Uploads a file to a given Cloud Storage bucket and returns the public url
+to the new object.
+"""
+def upload_file(image_file, public):
+
+    # TODO: Use the bucket to get a blob object
+
+    blob = bucket.blob(image_file.filename)
+
+    # END TODO
+
+    # TODO: Use the blob to upload the file
+
+    blob.upload_from_string(
+        image_file.read(),
+        content_type=image_file.content_type)
+
+    # END TODO
+
+    # TODO: Make the object public
+
+    if public:
+        blob.make_public()
+
+    # END TODO
+
+
+    # TODO: Modify to return the blob's Public URL
+
+    return blob.public_url
+
+    # END TODO
