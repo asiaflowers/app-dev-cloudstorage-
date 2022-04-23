1f0dc4580bd4f809baa5f36554322015a6b918a4
asia flowers <90466041+asiaflowers@users.noreply.github.com>
2022-04-23 17:25:35 -0500

    Update questions.py

--- start/quiz/webapp/questions.py
+++ start/quiz/webapp/questions.py
@@ -1,1 +1,50 @@
 
+
+# TODO: Import the storage module
+
+from quiz.gcp import datastore
+
+# END TODO
+
+"""
+uploads file into google cloud storage
+- upload file
+- return public_url
+"""
+def upload_file(image_file, public):
+    if not image_file:
+        return None
+
+    # TODO: Use the storage client to Upload the file
+    # The second argument is a boolean
+
+    
+
+    # END TODO
+
+    # TODO: Return the public URL
+    # for the object
+
+    return u''
+
+    # END TODO
+
+"""
+uploads file into google cloud storage
+- call method to upload file (public=true)
+- call datastore helper method to save question
+"""
+def save_question(data, image_file):
+
+    # TODO: If there is an image file, then upload it
+    # And assign the result to a new Datastore property imageUrl
+    # If there isn't, assign an empty string
+    
+    
+    
+
+    # END TODO
+
+    data['correctAnswer'] = int(data['correctAnswer'])
+    datastore.save_question(data)
+    return
